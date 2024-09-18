cf7819c8ff17c8a5194abce13df4e00625f6d004
HoGiaKham <hogiakham1705@gmail.com>
2024-09-18 14:27:31 +0700

    Add files via upload

---
+++ 2c/App.js
@@ -0,0 +1,11 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+import Screen2c from './components/AssetExample'
+
+
+export default function App() {
+  return <Screen2c />;
+}
